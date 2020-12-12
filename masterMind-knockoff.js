5d103f1b76f909e2587f88d3c3eb1b111935e534
Matthew <matthew.d.baker20@gmail.com>
2020-12-12 01:26:10 -0600

    added jquery, cleaned up code

--- masterMind-knockoff.js
+++ masterMind-knockoff.js
@@ -7,16 +7,13 @@
         ...colorKey, "olive", "purple", "grey", "lime", "cyan", "tan"
     ];
     //game help & description
-    let help = document.getElementById("help");
     let helpEnabled = false;
-    let description = document.getElementById("gameHelp");
-    let removeHelp = document.getElementById("removeHelp");
-    help.addEventListener("click", function (){
-        description.style.display = "block";
+    $("#help").on("click", function (){
+        $("#gameHelp").fadeIn(500);
         helpEnabled = true;
         if(helpEnabled){
-            removeHelp.addEventListener("click", function (){
-                description.style.display = "none";
+            $("#removeHelp").on("click", function (){
+                $("#gameHelp").fadeOut(250);
                 helpEnabled = false;
             });
         }
@@ -25,13 +22,12 @@
     let sequence = [];
     let hardMode = document.getElementById("increaseDiff");
     let hard = false;
-    let isHard = document.getElementById("isHardMode");
     let hardButtons = document.getElementsByClassName("hard");
     hardMode.addEventListener("click", function () {
         hard = true;
         hardMode.style.color = "#14bdeb";
         hardMode.style.background = "#0d151d";
-        isHard.innerText = "Enabled";
+        $("#isHardMode").text("Enabled");
         hardMode.disabled = true;
         for(let i = 0; i < hardButtons.length; i++){
             hardButtons[i].style.display = "inline-block";
@@ -40,19 +36,11 @@
 
     let won = false;
     let startGame = document.getElementById("newGame");
-    let showSolution = document.getElementById("done");
-    let gameWon = document.getElementById("winner");
     let count = 0;
-    //answer key
-    let answer1 = document.getElementById("key1");
-    let answer2 = document.getElementById("key2");
-    let answer3 = document.getElementById("key3");
-    let answer4 = document.getElementById("key4");
     //assert button
     let assert = document.getElementById("submit");
     assert.disabled = true;
     // class selects
-    let yourGuesses = document.getElementsByClassName("yourColor");
     let redResponses = document.getElementsByClassName("outputRed");
     for(let i = 0; i < redResponses.length; i++){
         redResponses[i].style.color = "red";
@@ -66,29 +54,6 @@
     let secondC = document.getElementsByClassName("c2");
     let thirdC = document.getElementsByClassName("c3");
     let fourthC = document.getElementsByClassName("c4");
-    //response
-    let begin = document.getElementById("start");
-    let response1 = document.getElementById("one");
-    let response1W = document.getElementById("oneWhite");
-    let response2 = document.getElementById("two");
-    let response2W = document.getElementById("twoWhite");
-    let response3 = document.getElementById("three");
-    let response3W = document.getElementById("threeWhite");
-    let response4 = document.getElementById("four");
-    let response4W = document.getElementById("fourWhite");
-    let response5 = document.getElementById("five");
-    let response5W = document.getElementById("fiveWhite");
-    let response6 = document.getElementById("six");
-    let response6W = document.getElementById("sixWhite");
-    let response7 = document.getElementById("seven");
-    let response7W = document.getElementById("sevenWhite");
-    let response8 = document.getElementById("eight");
-    let response8W = document.getElementById("eightWhite");
-    let response9 = document.getElementById("nine");
-    let response9W = document.getElementById("nineWhite");
-    let response10 = document.getElementById("ten");
-    let response10W = document.getElementById("tenWhite");
-    let newGame = document.getElementById("anotherRound");
     //game timer and game start
     let timer = document.getElementById("timer");
     let time = 0;
@@ -116,16 +81,15 @@
         for(let i = 0; i < buttonColors.length; i++){
             buttonColors[i].disabled = false;
         }
-        begin.innerText = "Sequence Generated";
+        $("#start").text("Sequence Generated");
         startGame.disabled = true;
         hardMode.disabled = true;
-        showSolution.addEventListener("click", function () {
-            answer1.innerHTML = sequence[0];
-            answer2.innerHTML = sequence[1];
-            answer3.innerHTML = sequence[2];
-            answer4.innerHTML = sequence[3];
+        $("#done").on("click", function () {
+            $("#key1").text(sequence[0]);
+            $("#key2").text(sequence[1]);
+            $("#key3").text(sequence[2]);
+            $("#key4").text(sequence[3]);
             clearInterval(intervalID);
-            timer.style.display = "block";
             for(let i = 0; i < buttonColors.length; i++){
                 buttonColors[i].disabled = true;
             }
@@ -135,15 +99,14 @@
             pickCount = 500;
         });
     });
-    newGame.addEventListener("click", function(){
+    $("#anotherRound").on("click", function(){
         window.location.reload();
     });
 
     let buttonColors = document.getElementsByClassName("selectors");
     let spots = document.getElementsByClassName("selectedColor");
     let pickCount = 0;
-    let clearGuesses = document.getElementById("delete");
-    clearGuesses.addEventListener("click", function (){
+    $("#delete").on("click", function (){
         if(guessSet.length > 0) {
             guessSet.pop();
             pickCount--;
@@ -240,44 +203,44 @@
         let third = guessSet[2];
         let fourth = guessSet[3];
         if (count === 0) {
-            response1.innerText = reds(first, second, third, fourth, newKey);
-            response1W.innerText = whites(first, second, third, fourth, newKey);
+            $("#one").text(reds(first, second, third, fourth, newKey));
+            $("#oneWhite").text(whites(first, second, third, fourth, newKey));
 
         } else if (count === 1) {
-            response2.innerText = reds(first, second, third, fourth, newKey);
-            response2W.innerText = whites(first, second, third, fourth, newKey);
+            $("#two").text(reds(first, second, third, fourth, newKey));
+            $("#twoWhite").text(whites(first, second, third, fourth, newKey));
 
         } else if (count === 2) {
-            response3.innerText = reds(first, second, third, fourth, newKey);
-            response3W.innerText = whites(first, second, third, fourth, newKey);
+            $("#three").text(reds(first, second, third, fourth, newKey));
+            $("#threeWhite").text(whites(first, second, third, fourth, newKey));
 
         } else if (count === 3) {
-            response4.innerText = reds(first, second, third, fourth, newKey);
-            response4W.innerText = whites(first, second, third, fourth, newKey);
+            $("#four").text(reds(first, second, third, fourth, newKey));
+            $("#fourWhite").text(whites(first, second, third, fourth, newKey));
 
         } else if (count === 4) {
-            response5.innerText = reds(first, second, third, fourth, newKey);
-            response5W.innerText = whites(first, second, third, fourth, newKey);
+            $("#five").text(reds(first, second, third, fourth, newKey));
+            $("#fiveWhite").text(whites(first, second, third, fourth, newKey));
 
         } else if (count === 5) {
-            response6.innerText = reds(first, second, third, fourth, newKey);
-            response6W.innerText = whites(first, second, third, fourth, newKey);
+            $("#six").text(reds(first, second, third, fourth, newKey));
+            $("#sixWhite").text(whites(first, second, third, fourth, newKey));
 
         } else if (count === 6) {
-            response7.innerText = reds(first, second, third, fourth, newKey);
-            response7W.innerText = whites(first, second, third, fourth, newKey);
+            $("#seven").text(reds(first, second, third, fourth, newKey));
+            $("#sevenWhite").text(whites(first, second, third, fourth, newKey));
 
         } else if (count === 7) {
-            response8.innerText = reds(first, second, third, fourth, newKey);
-            response8W.innerText = whites(first, second, third, fourth, newKey);
+            $("#eight").text(reds(first, second, third, fourth, newKey));
+            $("#eightWhite").text(whites(first, second, third, fourth, newKey));
 
         } else if (count === 8) {
-            response9.innerText = reds(first, second, third, fourth, newKey);
-            response9W.innerText = whites(first, second, third, fourth, newKey);
+            $("#nine").text(reds(first, second, third, fourth, newKey));
+            $("#nineWhite").text(whites(first, second, third, fourth, newKey));
 
         } else if (count === 9) {
-            response10.innerText = reds(first, second, third, fourth, newKey);
-            response10W.innerText = whites(first, second, third, fourth, newKey);
+            $("#ten").text(reds(first, second, third, fourth, newKey));
+            $("#tenWhite").text(whites(first, second, third, fourth, newKey));
         }
         firstC[count].innerHTML = first;
         firstC[count].style.color = guessSet[0];
@@ -289,7 +252,7 @@
         fourthC[count].style.color = guessSet[3];
         if(won){
             clearInterval(intervalID);
-            gameWon.style.display = "block";
+            $("#winner").fadeIn(100);
             timer.style.display = "block";
             timer.innerHTML = `Solved in ${time} seconds`;
         }
