6e33ef12c2aa6336cf9c11cfebb143576fc49430
Matthew <matthew.d.baker20@gmail.com>
2020-12-13 18:37:07 -0600

    refactored project structure, issue with gitHub pages hosting

--- masterMind-knockoff.js
+++ masterMind-knockoff.js
@@ -7,7 +7,7 @@
         ...colorKey, "olive", "purple", "grey", "lime", "cyan", "tan"
     ];
 
-
+    const postURL = "https://juniper-satisfying-cold.glitch.me/scores";
     function generateUser(name, mode, time, moves, arr){
         let ran = ~~(Math.random() * 80000);
         return {
